b14d51769b6c201f95be8f524d8ec13faced0e7d
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2023-10-06 14:15:52 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+// источник: http://digital-apple.ru/2017/01/22/java-zadacha-22-18-b1-tetris/
+
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
     }
 }
